ca80bf6287201f97d59dc830d4d095338040f901
Mudassir Latif <mudassirm.latif@gmail.com>
2025-09-17 13:32:23 +0500

    eslint config file

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    plugins: {},
+    rules: {},
+  },
+];
